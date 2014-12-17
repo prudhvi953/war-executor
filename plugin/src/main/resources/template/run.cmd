dcc2f26ac1b1c22f88fbb34223416fa078c13fc8
prudhvi krishna <prudhvi953@gmail.com>
2014-12-17 14:20:01 +0530

    Update run.cmd

--- plugin/src/main/resources/template/run.cmd
+++ plugin/src/main/resources/template/run.cmd
@@ -3,4 +3,4 @@ SET warPath='dir *.war'
 IF "$1"!="" SET port="$1"
 IF "$1"=="" SET port="8080"
 
-java -cp "env/*" com.imaginea.embeddedserver.EmbeddedServer $warPath $port
+java -cp "env/*" com.imaginea.embeddedserver.EmbeddedServer "%warPath%" "%port%"
